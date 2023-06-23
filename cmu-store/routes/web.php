c56075ef7471f8b3ec4f5ced7b18dfade4c7670f
DARKWOLF2517 <jerrichopamisa@gmail.com>
2023-06-23 19:13:22 +0800

    display events from dadtabase

--- cmu-store/routes/web.php
+++ cmu-store/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,4 +24,5 @@ Route::get('StudOrgEvents', function () {
 
 // Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/events', [EventController::class, 'showEvents'])->name('events');
