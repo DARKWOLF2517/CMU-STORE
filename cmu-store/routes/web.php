f26a4c37bffe3a63c80317d51ad3926556d31eaa
DARKWOLF2517 <jerrichopamisa@gmail.com>
2023-06-27 22:22:15 +0800

    added delete function on student org events

--- cmu-store/routes/web.php
+++ cmu-store/routes/web.php
@@ -36,8 +36,13 @@ Route::get('/dashboard', [HomeController::class, 'index'])->middleware(['auth', 
 Route::get('/logout', [HomeController::class, 'logout']);
 
 #events
-Route::post('/events', [EventController::class, 'store'])->name('events.store');
+
 Route::get('/StudOrgEvents', [EventController::class, 'showEvents'])->name('events');
+Route::post('/events', [EventController::class, 'store'])->name('events.store');
+Route::get('/events/{event}/edit', [EventController::class, 'edit'])->name('events.edit');
+Route::put('/events/{event}', [EventController::class, 'update'])->name('events.update');
+Route::delete('/events/{event}', [EventController::class, 'destroy'])->name('events.destroy');
+
 
 
 
