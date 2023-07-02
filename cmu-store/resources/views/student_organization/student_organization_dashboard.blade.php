57893b709fc9ca457313bb37815d807ced9785e3
ueng-lin <queenrbahay@gmail.com>
2023-07-02 20:06:54 +0800

    wad

--- cmu-store/resources/views/student_organization/student_organization_dashboard.blade.php
+++ cmu-store/resources/views/student_organization/student_organization_dashboard.blade.php
@@ -162,6 +162,57 @@
       },
     };
     </script>
+    <script>
+        window.addEventListener('DOMContentLoaded', (event) => {
+          const today = new Date();
+          let currentMonth = today.getMonth();
+          let currentYear = today.getFullYear();
+          const cells = document.getElementsByTagName('td');
 
+          function updateCalendar() {
+            const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            const firstDay = new Date(currentYear, currentMonth, 1);
+            const lastDay = new Date(currentYear, currentMonth + 1, 0);
+            const numDays = lastDay.getDate();
+            const startDay = firstDay.getDay();
+
+            for (let i = 0; i < cells.length; i++) {
+              cells[i].innerHTML = '';
+              cells[i].classList.remove('today');
+            }
+
+            document.getElementById('month-year').textContent = `${monthNames[currentMonth]} ${currentYear}`;
+
+            let date = 1;
+            for (let i = startDay; i < startDay + numDays; i++) {
+              cells[i].innerHTML = date;
+              if (date === today.getDate() && currentMonth === today.getMonth() && currentYear === today.getFullYear()) {
+                cells[i].classList.add('today');
+              }
+              date++;
+            }
+          }
+
+          updateCalendar();
+
+          document.getElementById('prev-month').addEventListener('click', () => {
+            currentMonth--;
+            if (currentMonth < 0) {
+              currentMonth = 11;
+              currentYear--;
+            }
+            updateCalendar();
+          });
+
+          document.getElementById('next-month').addEventListener('click', () => {
+            currentMonth++;
+            if (currentMonth > 11) {
+              currentMonth = 0;
+              currentYear++;
+            }
+            updateCalendar();
+          });
+        });
+      </script>
 
 @endsection
