72680024760ef0f2bb53e8b35fac9ac656c45ee2
DARKWOLF2517 <jerrichopamisa@gmail.com>
2023-07-02 19:07:04 +0800

    vue

--- cmu-store/resources/views/student/student_dashboard.blade.php
+++ cmu-store/resources/views/student/student_dashboard.blade.php
@@ -5,27 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sidebars/">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@ 5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="/custom_css/Navbars.css" rel="stylesheet">
     @vite('resources/js/app.js')
+
 </head>
 <body>
     <h1>Student Dashboard</h1>
-    <div id="app"class = " container">
-
-        <example-component/>
+    <div id="app"> 
         
-       
+        <navigation_bars/>
     </div>
+    
      
   
-    <ul id="app" class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
+    {{-- <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
         <li><a class="dropdown-item" href="#">Settings</a></li>
         <li><a class="dropdown-item" href="#">Profile</a></li>
         <li><hr class="dropdown-divider"></li>
         <li><a class="dropdown-item" href="{{url('/logout')}}">Sign out</a></li>
    
-        
-      </form>
-    </ul>
+      
+    </ul> --}}
 
 </body>
 </html>
