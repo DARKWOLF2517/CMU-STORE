3fc9bf4312be8c36194bdc8bbf947b5554dacb79
ueng-lin <queenrbahay@gmail.com>
2023-10-09 22:39:57 +0800

    10/9/2023

--- cmu-store/resources/views/student/student_event_details.blade.php
+++ cmu-store/resources/views/student/student_event_details.blade.php
@@ -1,63 +1,114 @@
 @extends('layouts.student_navigation_bar')
 @section('main-content')
-<link href="/custom_css/studentAccountabilities.css" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.16.9/xlsx.full.min.js"></script>
+
+<link href="/custom_css/StudentAccountabilities.css" rel="stylesheet">
 <div class="content">
     <div class="container">
         <div class="container breadcrumbs">
             <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="/login/stud_dashboard">Dashboard</a></li>
-                    <li class="breadcrumb-item">Student</li>
-                    <li class="breadcrumb-item">CSCo</li>
-                    <li class="breadcrumb-item active" aria-current="page">Events</li>
-                </ol>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="/login/org_dashboard">Dashboard</a></li>
+                <li class="breadcrumb-item">Student Organization</li>
+                <li class="breadcrumb-item active" aria-current="page">Accountabilities</li>
+            </ol>
             </nav>
         </div>
-            <div class="container" id="tablecontainer">
-                <div class="row head-container">
-                    <div class="col-md-6 col-sm-12">
-                        <div class="input-container">
-                            <i class="fa fa-search"></i>
-                            <input type="text" placeholder="Search Event">
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-sm-12" style="display: flex; align-items: center; justify-content: flex-end;">
-                        <button class="btn sort-btn"><i class="bi bi-sort-up"></i></button>
-                        <div class="select-dropdown">
-                            <select id="sort-select" class="form-control" style="text-align: center;">
-                                <option value="">Select Semester</option>
-                                <option value="option1">1st Semester 2023-2024</option>
-                                <option value="option2">2nd Semester 2022-2023</option>
-                                <option value="option3">1st Semester 2022-2023</option>
-                            </select>
-                        </div>
+        <div class="container" id="tablecontainer">
+            <div class="row head-container">
+                <div class="col-md-6 col-sm-12">
+                    <h3> <i class="fas fa-list"></i> Attendance</h3>
+                </div>
+                <div class="col-md-6 col-sm-12" style="display: flex; align-items: center; justify-content: flex-end;">
+                    <div class="select-dropdown">
+                        <select id="sort-select" class="form-control" style="text-align: center;">
+                            <option value="">Select Semester</option>
+                            <option value="option1">1st Semester 2023-2024</option>
+                            <option value="option2">2nd Semester 2022-2023</option>
+                            <option value="option3">1st Semester 2022-2023</option>
+                        </select>
                     </div>
                 </div>
             </div>
-            <div class="mt-4">
-                <h3> <i class="fas fa-list"></i>  Student Accountabilities</h3>
-            </div>
-            <div class="scroll-pane">
-            <table  id="accountabilities-table">
-                <thead>
+        </div>
+    <div class="scroll-attendance">
+        <div class="container" id="tablecontainer">
+            <h4> Event name: Palaro</h4>
+            <h6> Number of Days: 2</h6>
+            <h6> Total number of Attendance made: 4</h6>
+            <table class="mt-4">
+                <thead >
                     <tr>
-                        <th class="sortable-header">Student Name</th>
-                        <th class="sortable-header">Membership Fee Status</th>
-                        <th class="sortable-header">Fines Status</th>
-                        <th class="sortable-header">Overall status</th>
+                        <th class="sortable-header center" colspan="5">September 2, 2023</th>
+                    </tr>
+                    <tr>
+                        <th class="sortable-header">Log in - 7:00 AM</th>
+                        <th class="sortable-header"> Log out - 11:00 AM</th>
+                        <th class="sortable-header">Log in - 1:00 PM</th>
+                        <th class="sortable-header"> Log out - 4:00 PM</th>
+                        <th class="sortable-header"> Evaluation</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                    <td>John Smith</td>
-                    <td>Unpaid</td>
-                    <td>Unpaid</td>
-                    <td>Not cleared</td>
+                        <td class="present">Present</td>
+                        <td class="present">Present</td>
+                        <td class="absent">Absent</td>
+                        <td class="absent">Absent</td>
+                    <td><button class="btn btn-warning">Evaluate now</button></td>
                     </tr>
                 </tbody>
             </table>
+            <table class="mt-4">
+                <thead >
+                    <tr>
+                        <th class="sortable-header center" colspan="5">September 3, 2023</th>
+                    </tr>
+                    <tr>
+                        <th class="sortable-header">Log in - 7:00 AM</th>
+                        <th class="sortable-header"> Log out - 11:00 AM</th>
+                        <th class="sortable-header">Log in - 1:00 PM</th>
+                        <th class="sortable-header"> Log out - 4:00 PM</th>
+                        <th class="sortable-header"> Evaluation</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="present">Present</td>
+                        <td class="present">Present</td>
+                        <td class="absent">Absent</td>
+                        <td class="absent">Absent</td>
+                    <td><button class="btn btn-warning">Evaluate now</button></td>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
+            <div class="container" id="tablecontainer">
+                <h4> Event name: Acquaintance Party</h4>
+                <h6> Number of Days: 1</h6>
+                <h6> Total number of Attendance made: 1</h6>
+                <table class="mt-4">
+                    <thead >
+                        <tr>
+                            <th class="sortable-header center" colspan="5">September 2, 2023</th>
+                        </tr>
+                        <tr>
+                            <th class="sortable-header">Log in - 7:00 AM</th>
+                            <th class="sortable-header"> Log out - 11:00 AM</th>
+                            <th class="sortable-header"> Evaluation</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="present">Present</td>
+                            <td class="present">Present</td>
+                        <td><button class="btn btn-success">Evaluated</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+                </div>
         </div>
-        </div>
-
     </div>
-    </div>
+</div>
+</div>
+@endsection
