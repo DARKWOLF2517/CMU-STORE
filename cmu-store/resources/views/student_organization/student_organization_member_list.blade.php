aa42252f8c163f6919e65ace4b29c5cc76fa8f1d
ueng-lin <queenrbahay@gmail.com>
2023-11-04 09:37:12 +0800

    modal table

--- cmu-store/resources/views/student_organization/student_organization_member_list.blade.php
+++ cmu-store/resources/views/student_organization/student_organization_member_list.blade.php
@@ -64,8 +64,8 @@
                 <tr>
                     <th>Student ID</th>
                     <th>Name</th>
+                    <th>Institutional Email</th>
                     <th>Year Level</th>
-                    <th>College</th>
                     <th>Actions</th>
                 </tr>
             </thead>
@@ -100,6 +100,187 @@
     </div>
     </div>
 
+       <!-- Modal for displaying Excel data -->
+       <div class="modal fade" id="excelDataModal" tabindex="-1" aria-labelledby="excelDataModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="excelDataModalLabel">Excel Data</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Student ID</th>
+                                <th>Name</th>
+                                <th>Institutional Email</th>
+                                <th>Year Level</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody id="modalStudentTableBody">
+                            <!-- Excel data will be displayed here -->
+                        </tbody>
+                    </table>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" id="uploadToTableButton">Upload to Table</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+  <!-- Include Bootstrap and jQuery JavaScript -->
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+
+      <!-- Include exceljs for parsing Excel files -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/exceljs/4.4.0/exceljs.min.js"></script>
+
+      <script>
+          // Function to handle the file upload
+          document.getElementById("uploadButton").addEventListener("click", function () {
+              document.getElementById("fileInput").click();
+          });
+
+          document.getElementById("fileInput").addEventListener("change", function (e) {
+              const file = e.target.files[0];
+              if (file) {
+                  parseExcelData(file);
+              }
+          });
+
+          // Function to parse the uploaded Excel file and display it in the modal
+          async function parseExcelData(file) {
+              const workbook = new ExcelJS.Workbook();
+              const reader = new FileReader();
+
+              reader.onload = async function (e) {
+                  const buffer = e.target.result;
+                  await workbook.xlsx.load(buffer);
+                  const worksheet = workbook.getWorksheet(1); // Assuming data is in the first worksheet
+
+                  // Clear the existing modal table content
+                  const modalTableBody = document.getElementById("modalStudentTableBody");
+                  modalTableBody.innerHTML = "";
+
+                  // Populate the modal table with student data
+                  worksheet.eachRow({ includeEmpty: false }, function (row, rowNumber) {
+                      if (rowNumber === 1) return; // Skip the header row
+                      const student = row.values;
+
+                      const newRow = document.createElement("tr");
+                      newRow.innerHTML = `
+                          <td>${student[1]}</td>
+                          <td>${student[2]}</td>
+                          <td>${student[3]}</td>
+                          <td>${student[4]}</td>
+                      `;
+
+                      modalTableBody.appendChild(newRow);
+                  });
+
+                  // Show the modal
+                  const excelDataModal = new bootstrap.Modal(document.getElementById("excelDataModal"), { keyboard: false });
+
+                  excelDataModal.show();
+              };
+
+              reader.readAsArrayBuffer(file);
+          }
+
+          // Function to upload data from the modal to the main table
+  document.getElementById("uploadToTableButton").addEventListener("click", function () {
+      const modalTableBody = document.getElementById("modalStudentTableBody");
+      const tableBody = document.getElementById("studentTableBody");
+
+      // Clone the rows from the modal and append them to the main table with "Edit" and "Delete" buttons in the last cell
+      const modalRows = modalTableBody.querySelectorAll("tr");
+      modalRows.forEach(function (row) {
+          const newRow = row.cloneNode(true);
+
+          // Create a cell for "Edit" and "Delete" buttons in the last column
+          const actionsCell = document.createElement("td");
+
+          // Add "Edit" button
+          const editButton = document.createElement("button");
+          editButton.textContent = "Edit";
+          editButton.classList.add("btn", "btn-primary", "edit-button");
+          actionsCell.appendChild(editButton);
+
+          // Add "Delete" button
+          const deleteButton = document.createElement("button");
+          deleteButton.textContent = "Delete";
+          deleteButton.classList.add("btn", "btn-danger", "delete-button");
+          actionsCell.appendChild(deleteButton);
+
+          // Append the actions cell to the new row
+          newRow.appendChild(actionsCell);
+
+          // Append the new row to the main table
+          tableBody.appendChild(newRow);
+      });
+
+      // Hide the modal
+      const excelDataModal = new bootstrap.Modal(document.getElementById("excelDataModal"));
+      excelDataModal.hide();
+  });
+
+          // Function to handle the "Delete" button click
+          document.getElementById("studentTableBody").addEventListener("click", function (e) {
+              if (e.target && e.target.classList.contains("delete-button")) {
+                  const row = e.target.closest("tr");
+                  if (confirm("Are you sure you want to delete this student?")) {
+                      // If the user confirms the deletion, remove the row from the table
+                      row.remove();
+                  }
+              }
+          });
+
+          // Function to handle the "Edit" button click
+          document.getElementById("studentTableBody").addEventListener("click", function (e) {
+              if (e.target && e.target.classList.contains("edit-button")) {
+                  const row = e.target.closest("tr");
+                  const cells = row.querySelectorAll("td");
+
+                  // Disable the Edit button
+                  e.target.disabled = true;
+
+                  // Enable editing of the student details
+                  for (let i = 0; i < cells.length - 1; i++) {
+                      const cellContent = cells[i].textContent;
+                      const input = document.createElement("input");
+                      input.value = cellContent;
+                      cells[i].textContent = "";
+                      cells[i].appendChild(input);
+                  }
+
+                  // Create a Save button
+                  const saveButton = document.createElement("button");
+                  saveButton.textContent = "Save";
+                  saveButton.classList.add("btn", "btn-success");
+                  row.querySelector(".edit-button").insertAdjacentElement("beforebegin", saveButton);
+
+                  // Handle saving changes
+                  saveButton.addEventListener("click", function () {
+                      // Update the table with the edited details
+                      for (let i = 0; i < cells.length - 1; i++) {
+                          const input = cells[i].querySelector("input");
+                          cells[i].textContent = input.value;
+                      }
+
+                      // Remove the Save button and re-enable the Edit button
+                      saveButton.remove();
+                      e.target.disabled = false;
+                  });
+              }
+          });
+  </script>
+
+{{-- WALA NI LABOT ANG SA UBOS NA --}}
 
     <!-- Include Bootstrap and jQuery JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
