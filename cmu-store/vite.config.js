2e6152aa62f7df4bd677a547f2fe28ad5f171378
DARKWOLF2517 <jerrichopamisa@gmail.com>
2023-11-06 21:39:00 +0800

    asdf

--- cmu-store/vite.config.js
+++ cmu-store/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-
+import mkcert from 'vite-plugin-mkcert'
 export default defineConfig({
     plugins: [
+        mkcert() ,
         laravel({
             input: [
                 'resources/sass/app.scss',
